4ff24c5fe5fc007e35590dcec1f1fc46babd9332
Pipe <pipe.evc@gmail.com>
2022-05-12 03:44:07 -0400

    agregamos logger

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import os
 import datetime
+import logging
+import os
 
+import coloredlogs
 import randomname
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, insert
 from sqlalchemy.orm import Session
 
-from models import Cazadores
+from models import Cazadores, Ataques
 
-load_dotenv()  # take environment variables from .env.
+# create logger
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG')
+
+# take environment variables from .env.
+load_dotenv()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -24,27 +27,43 @@ if __name__ == '__main__':
     session = Session(engine)
 
     # obtener último ID de los cazadores
-    print("LastID:")
     lastId = session.query(Cazadores.id_cazador).order_by(Cazadores.id_cazador.desc()).first().id_cazador
     if lastId is None:
         lastId = 1
-    print(lastId)
+    logger.info("LastID Cazador=" + str(lastId))
 
     print()
-    print("Creamos cazadores...")
+    logger.info("Creamos cazadores...")
     for x in range(10):
         lastId = lastId + 1
-        stmt = insert(Cazadores).values(id_cazador=lastId, nombre=randomname.get_name() + str(lastId), rango='El mejor',
+        stmt = insert(Cazadores).values(id_cazador=lastId, nombre=randomname.get_name(), rango='El mejor',
                                         pais_origen='Chile',
                                         birthday=datetime.datetime.now())
         result = session.execute(stmt)
     session.commit()
 
     print()
-    print("Mostramos todos los cazadores...")
+    logger.info("Mostramos todos los cazadores...")
     for jugador in session.query(Cazadores).all():
-        print(jugador.nombre)
+        logger.info(jugador.nombre)
 
     print()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # obtener último ID de los ataques
+    lastId = session.query(Ataques.id_ataque).order_by(Ataques.id_ataque.desc()).first().id_ataque
+    if lastId is None:
+        lastId = 1
+    logging.info("LastID Ataque=" + str(lastId))
+
+    logger.info("Creamos ataques...")
+    for x in range(10):
+        lastId = lastId + 1
+        stmt = insert(Ataques).values(id_ataque=lastId, nombre_ataque=randomname.get_name(), arma=randomname.get_name())
+        result = session.execute(stmt)
+    session.commit()
+
+    logger.info("Mostramos todos los ataques...")
+    for ataque in session.query(Ataques).all():
+        logger.info(ataque.nombre_ataque)
+
+    print()
